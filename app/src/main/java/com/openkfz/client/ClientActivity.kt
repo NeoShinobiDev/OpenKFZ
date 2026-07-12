ab4ea8b65695b615fdbd8dcba3fd3754e17bccb2
NeoShinobiDev <patrickrenges975@gmail.com>
2026-07-12 19:27:43 +0200

    Create client fullscreen compose UI

--- app/src/main/java/com/openkfz/client/ClientActivity.kt
+++ app/src/main/java/com/openkfz/client/ClientActivity.kt
@@ -1,17 +1,68 @@
 package com.openkfz.client
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import com.openkfz.R
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.openkfz.app.R
 
-class ClientActivity : AppCompatActivity() {
+class ClientActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContent {
+            ClientScreen()
+        }
+    }
+}
 
-        setContentView(R.layout.activity_client)
+
+@Composable
+fun ClientScreen() {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+
+        // Kamera kommt später hier rein
+
+
+        FloatingActionButton(
+            onClick = {},
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 32.dp),
+            containerColor = Color.White
+        ) {
+
+        }
+
+
+        IconButton(
+            onClick = {},
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(24.dp)
+        ) {
+
+            Icon(
+                imageVector = Icons.Default.Settings,
+                contentDescription = "Settings",
+                tint = Color.White
+            )
+
+        }
     }
 }
